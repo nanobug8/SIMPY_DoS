7906057209fa4569928b93e25ec55ab65382fd24
nanobug8 <52039127+nanobug8@users.noreply.github.com>
2019-06-29 23:39:40 -0300

    Update DoS.py

--- DoS.py
+++ DoS.py
@@ -112,10 +112,10 @@ print (" ---------------------------------------------------------------------")
 
 labels = 'DoS time', 'Operative time'
 sizes = [G.DoS.mean(),G.success.mean()]
-explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+explode = (0.1, 0)
 
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.axis('equal')
 
 plt.show()
